c4cbd0f13931a8bcc1ae4d719dddcb94e9604a28
Erick <emorales15b@gmail.com>
2018-04-09 11:12:43 -0500

    cambio de titulo contador

--- Inicio.js
+++ Inicio.js
@@ -29,11 +29,7 @@ class Inicio extends React.Component{
         return (
             <View style={styles.container} >
                 <View style={styles.cajaUno}>
-<<<<<<< HEAD
-                    <Text>Contadors</Text>
-=======
                     <Text>Titulo Contador</Text>
->>>>>>> test
                 </View>
                 <View style={styles.cajaDos}>
                  {/*   <Text>2</Text>*/}
