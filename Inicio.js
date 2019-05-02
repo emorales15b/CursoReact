6f5a23da31420c8508544f1cdeed35128c2f40f1
Erick <emorales15b@gmail.com>
2019-05-02 15:53:49 -0700

    otro test12

--- Inicio.js
+++ Inicio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-//export default aadgffgdasdsdf
+//export default aadgffgdasdsdfdgf
 class Inicio extends React.Component{
 
 
